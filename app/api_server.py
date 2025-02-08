e3985d3ddbbf5c7c3acf5b2b69dd4c781b74e1f2
Dallas Klein <dallasjklein@gmail.com>
2025-02-08 16:42:26 -0600

    certs

--- app/api_server.py
+++ app/api_server.py
@@ -176,7 +176,7 @@ if __name__ == "__main__":
         uvicorn.run(
             app,
             host="0.0.0.0",
-            port=8443,  # Standard HTTPS port
+            port=8000,  # Standard HTTPS port
             ssl_keyfile=ssl_keyfile,
             ssl_certfile=ssl_certfile
         ) 
